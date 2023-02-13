cdba6bff38b30b85d35b1b4ccb8801dab94adc4b
Moses Lusengeri Anzagi <anzagi.moses@eclectics.io>
2023-02-13 21:44:54 +0300

    add cluster security group

--- infrastructure/modules/basic_networking/security_groups.tf
+++ infrastructure/modules/basic_networking/security_groups.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "alb_sg" {
 
   ingress {
     description = "allow all connections on port 6443 from internet"
-    from_port   = 6443 
-    to_port     = 6443 
+    from_port   = 6443
+    to_port     = 6443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -19,5 +19,29 @@ resource "aws_security_group" "alb_sg" {
 
   tags = {
     Name = "${var.namespace}-alb-sg"
+  }
+}
+
+resource "aws_security_group" "cluster_sg" {
+  name   = "${var.namespace}-cluster-sg"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description = "allow all connections on port 6443 from alb_sg"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.namespace}-cluster-sg"
   }
 }
